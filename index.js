2a914cb76540f3752473015c347e620bb99341ee
pychangeric <pychangeric@gmail.com>
2023-03-08 18:55:35 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,21 @@
+function shout (string) {
+    return string.toUpperCase();
+}
+function whisper (string) {
+    return string.toLowerCase();
+}
+function logshout (hello) {
+    console.log(hello.toUpperCase( ));
+}
+function logwhisper (HELLO) {
+    console.log (HELLO.toLowerCase());
+}
+function sayHiToHeadphoneRoomate (HELLO)
+ if (HELLO.toLowerCase) {
+    console.log("I can\'t hear you!")
+ }else if (HELLO.toUpperCase){
+    console.log("yes indeed!")
+ }else ("let\'s have dinner together!"){
+    console.log("I would love to!")
+ }
+   
